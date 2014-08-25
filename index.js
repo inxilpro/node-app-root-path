f0858cb42267a70db2ac04969d1a9ef8cdd64184
Chris Morrell <github@rchy.net>
2014-08-25 17:02:26 -0400

    Split public interface off into its own file for easier testing

--- index.js
+++ index.js
@@ -1,32 +1,4 @@
 'use strict';
 
-var path = require('path');
-var resolve = require('./lib/resolve.js');
-var appRootPath = resolve(__dirname);
-
-exports.resolve = function(pathToModule) {
-	return path.join(appRootPath, pathToModule);
-};
-
-exports.require = function(pathToModule) {
-	// Backwards compatibility check
-	if ('function' === typeof pathToModule) {
-		console.warn('Just use appRootPath.require() -- no need to pass in your ' +
-					 'modules\'s require() function any more.');
-		return function(pathToModule) {
-			return exports.require(pathToModule);
-		}
-	}
-
-	return require(exports.resolve(pathToModule));
-};
-
-exports.toString = function() {
-	return appRootPath;
-};
-
-exports.setPath = function(explicitlySetPath) {
-	appRootPath = path.resolve(explicitlySetPath);
-}
-
-exports.path = appRootPath;
+var lib = require('./lib/app-root-path.js');
+module.exports = lib(__dirname);
--- test/index.js
+++ test/index.js
@@ -2,9 +2,10 @@
 
 var path = require('path');
 var assert = require('assert');
-var resolve = require('../lib/resolve.js');
 
-describe('The path resolution method...', function() {
+describe('The path resolution method', function() {
+	var resolve = require('../lib/resolve.js');
+
 	// Check global paths
 	it('should use require.main.filename if the path is in the globalPaths array', function() {
 		var expected = path.dirname(require.main.filename);
@@ -47,7 +48,45 @@ describe('The path resolution method...', function() {
 	// Check when setting via environmental variable
 	it('should respect the APP_ROOT_PATH environmental variable', function() {
 		var expected = '/some/arbirary/path';
+		var originalPath = process.env.APP_ROOT_PATH;
 		process.env.APP_ROOT_PATH = expected;
 		assert.equal(resolve('/somewhere/else'), expected);
+		process.env.APP_ROOT_PATH = originalPath;
+	});
+});
+
+describe('The public interface', function() {
+	var lib = require('../lib/app-root-path.js');
+	var root = path.resolve(__dirname);
+	var pub = lib(root + '/node_modules/app-root-path');
+
+	it('should expose a resolve() method that resolves a relative path to the root path', function() {
+		assert(pub.resolve);
+		assert.equal(pub.resolve('subdir/filename.js'), root + '/subdir/filename.js');
+	});
+
+	it('should expose a require() method that properly loads a module relative to root', function() {
+		assert(pub.require);
+		var testlib = pub.require('lib/testlib.js');
+		assert.equal(testlib, 'hello world');
+	});
+
+	it('should implement toString()', function() {
+		assert(pub.toString);
+		assert.equal(pub + '', root);
+		assert.equal(pub.toString(), root);
+	});
+
+	it('should allow explicitly setting the root path with setPath()', function() {
+		assert(pub.setPath);
+		var originalPath = pub.toString();
+		pub.setPath('/path/to');
+		assert.equal(pub.resolve('somewhere'), '/path/to/somewhere');
+		pub.setPath(originalPath);
+	});
+
+	it('should expose the app root path as a .path property', function() {
+		assert(pub.path);
+		assert.equal(pub.path, pub.toString());
 	});
 });
